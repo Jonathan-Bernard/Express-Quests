07853db41cf6d020a98003a2c6207d563567be44
Jonathan-Bernard <johnnyboy59000@gmail.com>
2023-11-29 10:44:47 +0100

    Express 03

--- src/app.js
+++ src/app.js
@@ -6,11 +6,17 @@ app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
 const userControllers = require("./controllers/userControllers");
+
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUsersById);
+
 app.post("/api/users", userControllers.postUser);
 app.post("/api/movies", movieControllers.postMovie);
+
+app.put("/api/movies/:id", movieControllers.updateMovie);
+app.put("/api/users/:id", userControllers.updateUser);
 
 module.exports = app;
